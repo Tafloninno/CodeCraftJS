a4560eef8a1615b9f57b05b4e63dbace4a875552
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2024-05-09 17:29:27 -0400

    display functionality

--- 5/index.js
+++ 5/index.js
@@ -1,10 +1,44 @@
-const expenses = document.getElementById("expenses")
+let expenses = []
+const input = document.getElementById("expensesInput")
+// const priceInput = documnet.getElementById("price")
+const expensesDiv = document.getElementById("expenses")
+
 
 function displayExpenses(){
-    
+    expensesDiv.innerHTML = null;
+
+    for(const [idx, expense] of Object.entries(expenses)){
+        const container = document.createElement('div')
+
+        const text = document.createElement('p')
+        text.textContent = expense
+
+        const button = document.createElement('button')
+        button.textContent = "delete"
+
+        container.appendChild(text)
+        container.appendChild(button)
+
+        expensesDiv.appendChild(container)
+    }
 }
 
-function deleteExpenses(){}
+function addExpenses(){
+    event.preventDefault();
+    const value = input.value
+    if(!value){
+        alert('Fill in the expenses')
+        return
+    }else{
+        expenses.push(value)
+        displayExpenses()
+        input.value = ""
+    }
+
+}
+
+
+
 
 
 
